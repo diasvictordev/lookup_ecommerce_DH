d6a5286abd8115c4c428ec3a44de30c16bce5b49
rodrrigodev <rodrigosanyy@gmail.com>
2022-08-08 11:14:34 -0300

    new menu pages

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -1,6 +1,24 @@
 const menuController = {
     smartphones: (req, res)=>{
         res.render('smartphones')
+    },
+    perifericos: (req, res)=>{
+        res.render('perifericos')
+    },
+    audio: (req, res)=>{
+        res.render('audio')
+    },
+    cadeiras: (req, res)=>{
+        res.render('cadeiras')
+    },
+    hardware: (req, res)=>{
+        res.render('hardware')
+    },
+    tablet: (req, res)=>{
+        res.render('tablet')
+    },
+    tv: (req, res)=>{
+        res.render('tv')
     }
 }
 
