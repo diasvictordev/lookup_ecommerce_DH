30fd9db4ed640997c64690d2e6908e582f31060e
Victor <victor.dias@aluno.ueg.br>
2022-08-09 09:59:53 -0300

    add new models

---
+++ src/database/models/Product.js
@@ -0,0 +1,53 @@
+module.exports = (sequelize, datatypes)=>{
+    const alias = "Product"
+
+    const collumns = {
+        product_id: {
+            type: datatypes.STRING,
+            allowNull: false
+          },
+          price: {
+            type: datatypes.INTEGER,
+            allowNull: false
+          },
+          category: {
+            type: datatypes.STRING,
+            allowNull: false
+          },
+          amount: {
+            type: datatypes.INTEGER,
+            allowNull: false
+          },
+          state:{
+            type: datatypes.STRING,
+            allowNull: false
+          },
+          name: {
+            type: datatypes.STRING,
+            allowNull: false
+          },
+          description: {
+            type: dataypes.STRING,
+            allowNull: false
+          },
+          brand: {
+            type: datatypes.STRING,
+            allowNull: false
+          }
+    }
+
+    const config = {
+        tableName: "product",
+        timestamps: false
+    }
+
+    const Product = sequelize.define(alias, collumns, config)
+
+    Product.associate = (models)=>{
+        Product.hasMany(models.Cart, {
+            foreignKey: 'product_id', as : 'product_id'
+        })
+    }
+
+    return Product;
+}
