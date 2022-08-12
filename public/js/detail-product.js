9d272514b12dfe917dfdf1577439ec640f31b375
rodrrigodev <rodrigosanyy@gmail.com>
2022-08-12 11:36:59 -0300

    nem functions

--- public/js/detail-product.js
+++ public/js/detail-product.js
@@ -1,5 +1,5 @@
 function imageFunction(imageFrame) {
     let fullImage = document.getElementById("imageBox");
-    fullImage.src = imageFrame.src;
+    return fullImage.src = imageFrame.src;
 
 }
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index);
+
 router.get('/carrinho', mainController.carrinho);
+
+router.get('/detalhe', mainController.detail);
 
 
 module.exports = router;
