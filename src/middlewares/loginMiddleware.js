3ffcb892e15450a01f5bbaaf508b10c7e8f2059e
rodrrigodev <rodrigosanyy@gmail.com>
2022-08-27 23:46:03 -0300

    cart info

--- src/middlewares/loginMiddleware.js
+++ src/middlewares/loginMiddleware.js
@@ -1,16 +1,28 @@
-const loginMiddleware = (req, res, next) => {
-  if(!req.session.user && !req.cookies.logado) {
-      return res.redirect('/usuario/login');
-  }
-  const user = req.session.user ? 
-      JSON.parse(req.session.user) : 
-      JSON.parse(req.cookies.logado)
+// const loginMiddleware = (req, res, next) => {
+//   if(!req.session.user && !req.cookies.logado) {
+//       return res.redirect('/usuario/login');
+//   }
+//   const user = req.session.user ? 
+//       JSON.parse(req.session.user) : 
+//       JSON.parse(req.cookies.logado)
 
-      // console.log(user)
 
-  req.user = user
+// module.exports = loginMiddleware
 
-  next()
-}
 
-module.exports = loginMiddleware
+//       // console.log(user)
+
+//   req.user = user
+
+//   next()
+// }
+
+
+function auth(req, res, next) {
+  if (req.session.user !== undefined) {
+  return next();
+  }else{
+    res.redirect('/usuario/login')
+}}
+
+module.exports = auth
